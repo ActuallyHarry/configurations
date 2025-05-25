3eacd357ba80f17518711e0429cc48a75bb18aae
ActuallyHarry <actuallyadequate@gmail.com>
2025-05-25 16:28:58 +1000

    authentik

---
+++ machines/vanguard/configuration.nix
@@ -0,0 +1,54 @@
+############################################################
+# Vanguard  Machine
+############################################################
+{ config, pkgs, ... }:
+{
+  imports = [
+    # Hardware
+    ./hardware-configuration.nix
+    # Modules
+    ../../modules/admin.nix
+    ../../modules/keyboard.nix
+    ../../modules/localisation.nix
+    ../../modules/nix_features.nix
+    ../../modules/ssl_wildcard.nix
+    ../../modules/nix_store.nix
+    ../../modules/sops.nix
+    ../../modules/ssl_wildcard.nix
+    # Applications
+    ../../applications/git.nix
+    ../../applications/ssh.nix
+   # ../../applications/vaultwarden.nix
+    ../../applications/authentik/authentik.nix
+  ];
+
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
+
+  networking.hostName = "vanguard";
+  networking.useDHCP = false;
+  networking.interfaces = {
+    ens18 = {
+      ipv4.addresses = [ {
+        address = "192.168.1.3";
+        prefixLength = 24;
+      } ];
+    };
+  };
+  networking.defaultGateway = "192.168.1.1";
+  networking.nameservers = [ "192.168.1.2 192.168.1.1"];
+  networking.networkmanager.enable = true;
+
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11";
+
+  
+
+}
