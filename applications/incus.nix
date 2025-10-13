baa575ad85f45605274c0593617825545721ddc1
ActuallyHarry <actuallyadequate@gmail.com>
2025-10-13 20:37:19 +1100

    basic configuration

--- applications/incus.nix
+++ applications/incus.nix
@@ -1,24 +1,31 @@
-{config, pkgs, lib, ...}:
-let
-  cfg = config.homeIncus;
-in
 {
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  cfg = config.homeIncus;
+in {
   options.homeIncus = {
     hostname = lib.mkOption {
-       type = lib.types.str;
-       description = "Hostname of the Hypervisor";
+      type = lib.types.str;
+      description = "Hostname of the Hypervisor";
     };
     ipv4 = lib.mkOption {
       type = lib.types.str;
       description = "IP of the hypervisor";
     };
     machineId = lib.mkOption {
-       type = lib.types.str;
-       description = "8 chars from /etc/machine-id";
+      type = lib.types.str;
+      description = "8 chars from /etc/machine-id";
     };
   };
 
-
+  config.boot.kernelModules = ["kvm-amd" "kvm-intel"]; # Include both for compatibility
+  config.boot.supportedFilesystems = ["zfs"];
+  config.networking.hostId = cfg.machineId;
+  # This option is necessary to ensure the thin-provisioning-tools
+  # binaries (like thin_check, which LVM uses) are correctly patched
 
   # This option ensures that dm-snapshot and dm-thin-pool are included
   # in the initrd, which is necessary for LVM thin provisioning to work
@@ -28,9 +35,6 @@ in
     "dm-thin-pool"
   ];
 
-  config.boot.kernelModules = [ "kvm-amd" "kvm-intel" ]; # Include both for compatibility
-  config.boot.supportedFilesystems = [ "zfs" ];
-  config.networking.hostId = cfg.machineId;
   # This option is necessary to ensure the thin-provisioning-tools
   # binaries (like thin_check, which LVM uses) are correctly patched
   # and accessible for LVM operations.
@@ -39,7 +43,6 @@ in
   config.virtualisation.incus.enable = true;
   config.virtualisation.incus.ui.enable = true;
   config.virtualisation.libvirtd.enable = true;
-
 
   config.networking.nftables.enable = true;
 
@@ -54,17 +57,17 @@ in
   ];
 
   config.virtualisation.incus.package = pkgs.incus.overrideAttrs (
-    old : {
+    old: {
       propagatedBuildInputs = old.propagatedBuildInputs or [] ++ [pkgs.zfs pkgs.openvswitch pkgs.lvm2 pkgs.thin-provisioning-tools];
-   });
+    }
+  );
 
-  config.networking.firewall.allowedTCPPorts = [ 443 8443 7443 53 67 ];
+  config.networking.firewall.allowedTCPPorts = [443 8443 7443 53 67];
 
   config.services.nginx.enable = true;
-  config.services.nginx.virtualHosts."${cfg.hostname}.home.actuallyharry.net" = {
-
-    serverName=cfg.hostname;
-    serverAliases=["${cfg.hostname}.home.actuallyharry.net"];
+  config.services.nginx.virtualHosts."${cfg.hostname}.zitohouse.net" = {
+    serverName = cfg.hostname;
+    serverAliases = ["${cfg.hostname}.zitohouse.net"];
     enableACME = false; # I am managing it not nginx
     forceSSL = true;
 
@@ -72,7 +75,7 @@ in
     sslCertificateKey = "/var/lib/acme/home-wildcard/key.pem";
 
     extraConfig = ''
-       add_header Strict-Tansport-Security "max-age=6307200" always;
+      add_header Strict-Tansport-Security "max-age=6307200" always;
     '';
 
     locations."/" = {
@@ -85,9 +88,7 @@ in
         proxy_set_header Host $host;
       '';
     };
-
   };
 
-  config.users.users.nginx.extraGroups = [ "acme" ];
-
+  config.users.users.nginx.extraGroups = ["acme"];
 }
--- machines/centurion/configuration.nix
+++ machines/centurion/configuration.nix
@@ -1,8 +1,11 @@
 ############################################################
 # {Hostname} Machine
 ############################################################
-{ config, pkgs, ... }:
 {
+  config,
+  pkgs,
+  ...
+}: {
   imports = [
     # Hardware
     ./hardware-configuration.nix
@@ -22,11 +25,10 @@
 
   #Incus Settings
   homeIncus = {
-     hostname = "centurion";
-     ipv4 = "192.168.90.253";
-     machineId = "c466cbf8";
+    hostname = "centurion";
+    ipv4 = "192.168.90.253";
+    machineId = "c466cbf8";
   };
-
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
@@ -35,25 +37,18 @@
   networking.useDHCP = false;
   networking.interfaces = {
     enp0s31f6 = {
-       useDHCP = false;
-    };
-    br0 = {
-      ipv4.addresses = [ {
-        address = "192.168.90.253";
-        prefixLength = 16;
-      } ];
+      useDHCP = false;
     };
   };
 
   networking.bridges.br0.interfaces = ["enp0s31f6"];
- 
-  networking.defaultGateway = {
-    address= "192.168.0.1";
-    interface= "br0";
-  };
-  networking.nameservers = [ "192.168.10.2 192.168.0.1"];
-  networking.networkmanager.enable = true;
 
+  networking.defaultGateway = {
+    address = "192.168.0.1";
+    interface = "br0";
+  };
+  networking.nameservers = ["192.168.10.2 192.168.0.1"];
+  networking.networkmanager.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -62,7 +57,4 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05";
-
-  
-
 }
