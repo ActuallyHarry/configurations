35b3087fe5f5c6f4a1ed5fa8a2b353161b553aee
ActuallyHarry <actuallyadequate@gmail.com>
2025-10-21 18:00:44 +1100

    postfix settings update

--- applications/postfix.nix
+++ applications/postfix.nix
@@ -13,27 +13,25 @@
     settings.main = {
        relayHost = ["smtp.gmail.com:587"];    
        mynetworks = ["127.0.0.0/8" "192.168.1.0/24" "192.168.10.0/24" "192.168.20.0/24" "192.168.90/24"];
-   
-    };
-    config = {
-     inet_interfaces = "all";
-     inet_protocols = "ipv4";
+    
+      inet_interfaces = "all";
+      inet_protocols = "ipv4";
      
-     # Outgoing Mail  
-     smtp_use_tls = "yes";
-     smtp_sasl_auth_enable = "yes";
-     smtp_sasl_security_options = "";
-     smtp_sasl_password_maps = "texthash:${config.sops.secrets."postfix/sasl_passwd".path}";
+      # Outgoing Mail  
+      smtp_use_tls = "yes";
+      smtp_sasl_auth_enable = "yes";
+      smtp_sasl_security_options = "";
+      smtp_sasl_password_maps = "texthash:${config.sops.secrets."postfix/sasl_passwd".path}";
 
-     # Incoming Mail
-     smtpd_use_tls = "yes";
-     smtpd_tls_cert_file = "/var/lib/acme/home-wildcard/cert.pem";
-     smtpd_tls_key_file = "/var/lib/acme/home-wildcard/key.pem"; 
+      # Incoming Mail
+      smtpd_use_tls = "yes";
+      smtpd_tls_cert_file = "/var/lib/acme/home-wildcard/cert.pem";
+      smtpd_tls_key_file = "/var/lib/acme/home-wildcard/key.pem"; 
 
 
     };
 
-    masterConfig = {
+    settings.master = {
       submission = {
         type = "inet";
         command = "smtpd";
