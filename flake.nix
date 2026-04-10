b9e70624a0b04bd676b62681cfdc5d9290ab3a6e
ActuallyHarry <actuallyadequate@gmail.com>
2026-04-10 20:40:21 +1000

    Machine auxilium (#18)
    * initial centurion
    
    * basic configuration
    
    * auxilium hypervisor
    
    * incus on auxilium
    
    * auxilium update

--- flake.nix
+++ flake.nix
@@ -28,6 +28,7 @@
     };
   };
 
+<<<<<<< HEAD
   outputs = {
     self,
     nixpkgs,
@@ -44,6 +45,55 @@
       inherit system;
       config = {
         allowUnfree = true;
+=======
+  outputs = { self, nixpkgs, sops-nix, ... }@ inputs: 
+    let
+      system =  "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config = { 
+          allowUnfree = true;
+        };
+      };
+    in  {
+
+      #####################################################################
+      # MACHINES
+      #####################################################################
+      
+      nixosConfigurations = {
+
+	# Hypervisor Hosts
+        centurion = nixpkgs.lib.nixosSystem {
+          specialArgs = {inherit inputs; inherit system; };
+
+          modules = [
+            ./machines/centurion/configuration.nix
+          ];
+        };
+
+        auxilium = nixpkgs.lib.nixosSystem {
+          specialArgs = {inherit inputs; inherit system; };
+
+          modules = [
+           ./machines/auxilium/configuration.nix
+          ];
+        };
+
+        # Network Manager
+        sentinel = nixpkgs.lib.nixosSystem {
+          specialArgs = {inherit inputs; inherit system; };
+          modules = [ ./machines/sentinel/configuration.nix ];
+        };
+
+        vanguard  = nixpkgs.lib.nixosSystem {
+          specialArgs = {inherit inputs; inherit system; };
+
+          modules = [
+            ./machines/vanguard/configuration.nix
+          ];
+        };
+>>>>>>> 001c2f9 (auxilium hypervisor)
       };
     };
   in {
