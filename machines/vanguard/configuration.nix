4192498472eda2814409da84eab3c222f5fd695b
ActuallyHarry <actuallyadequate@gmail.com>
2026-09-19 08:41:44 +1000

    net change

--- machines/vanguard/configuration.nix
+++ machines/vanguard/configuration.nix
@@ -31,12 +31,12 @@
     eth0 = {
       ipv4.addresses = [ {
         address = "192.168.10.3";
-        prefixLength = 16;
+        prefixLength = 24;
       } ];
     };
   };
-  networking.defaultGateway = "192.168.0.1";
-  networking.nameservers = [ "192.168.10.2 192.168.0.1"];
+  networking.defaultGateway = "192.168.10.1";
+  networking.nameservers = [ "192.168.10.2"];
   networking.networkmanager.enable = true;
 
 
